635677edeb658c80ded3e99bf22063a70febbc5d
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-28 11:56:33 -0300

    populated portfolioContainer page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import NavTabs from "./NavTabs";
+import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
 
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("About Me");
 
-export default function PortfolioContainer(){
-    return(
-        <div>
-            <NavTabs />
-        </div>
-    )
-} 
+  const renderPage = () => {
+    // return the corresponding component depending on the value of currentPage
+    if (currentPage === "About Me") {
+      return <AboutMe />;
+    }
+  };
+  if (currentPage === "Portfolio") {
+    return <Portfolio />;
+  }
+  if (currentPage === "Contact") {
+    return <Contact />;
+  }
+  if (currentPage === "Resume") {
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage;
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange= {handlePageChange}/>
+      {renderPage()}
+    </div>
+
+  );
+}
