d039223b86cd7486c15f7d7e045a97dbf37e2404
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-28 11:33:15 -0300

    created portfolio pages and folder structure

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavTabs from "./NavTabs";
+
+
+export default function PortfolioContainer(){
+    return(
+        <div>
+            <NavTabs />
+        </div>
+    )
+} 
