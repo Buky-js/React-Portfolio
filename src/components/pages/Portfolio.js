29346a22ebf3089bef57a0ca3cddf4de3337405c
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-28 16:46:55 -0300

    included cards in the portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,55 @@
 import React from "react";
 
 export default function Portfolio(){
-return(
-    <h1>Testing portfolio</h1>
+return (
+   
+<div className="row row-cols-1 row-cols-md-2 g-4">
+      <div className="col">
+        <div className="card">
+          <img src="images/events.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="images/fifa-world-cup.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="images/weather.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="images/notepad.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="images/runbuddy.png" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
 )
 };
