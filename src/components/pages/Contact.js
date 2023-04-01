67dccc7a484d8ca27f26659854765f56a52ec58c
Olubukola Abiona <bukyabiona@gmail.com>
2023-04-01 13:36:59 -0300

    modified the contact form to validate entries

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,50 +1,78 @@
 import React, { useState } from "react";
-import checkEmail from "../../utils/helpers";
+import { checkEmail } from "../../utils/helpers";
 
 export default function Contact() {
-  
-  
-  function formHandler(e){
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleEmpty(e) {
+    if (e.target.name === "email") {
+      const isValid = checkEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is not valid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (e.target.name === "name" || e.target.name === "message") {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+  }
+
+  function formHandler(e) {
     e.preventDefault();
   }
-  
-  
-  
-  
-    return (
-    <div className="col-lg-8 col-md-8 col-sm-8 container justify-content-center">
-      <div className="mt-5">
-        <h2>Contact Me</h2>
+
+  return (
+    <div className="row">
+      <div className="col-lg-8 col-md-8 col-sm-8 container justify-content-center">
+        <div className="mt-5">
+          <h2>Contact Me</h2>
+        </div>
+        <form onSubmit={formHandler} className="justify-content-center">
+          <div className="form-group col-md-6">
+            <label for="InputName">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Enter name"
+              onBlur={handleEmpty}
+            />
+          </div>
+
+          <div className="form-group col-md-6 mt-3">
+            <label for="InputEmail">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              placeholder="Enter email"
+              onBlur={handleEmpty}
+            />
+          </div>
+
+          <div className="form-group col-md-6 mt-3">
+            <label for="message">Message</label>
+            <textarea
+              className="form-control"
+              name="message"
+              rows="3"
+              onBlur={handleEmpty}
+            ></textarea>
+          </div>
+          {errorMessage && (
+            <div>
+              <p>{errorMessage}</p>
+            </div>
+          )}
+          <button type="submit" className="btn btn-primary mt-2 ">
+            Submit
+          </button>
+        </form>
       </div>
-      <form onSubmit={formHandler} className="justify-content-center">
-        <div className="form-group col-md-6">
-          <label for="InputName">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="InputName"
-            placeholder="Enter name"
-          />
-        </div>
-        <div className="form-group col-md-6 mt-5">
-          <label for="InputEmail">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="InputEmail"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group col-md-6 mt-5">
-          <label for="message">Message</label>
-          <textarea
-            className="form-control"
-            id="message"
-            rows="3"
-          ></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary mt-5">Submit</button>
-      </form>
     </div>
   );
 }
