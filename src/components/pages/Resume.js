e390fbd87f0fa84e353cb9d785f1a9c2addf6c43
Olubukola Abiona <bukyabiona@gmail.com>
2023-04-01 19:17:43 -0300

    modified the resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,17 +1,92 @@
 import React from "react";
-import myResume from "../../utils/MyResume.pdf"
+import myResume from "../../utils/MyResume.pdf";
+import { Container, Row, Col, Accordion, ListGroup, Button } from "react-bootstrap";
+import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
 
-export default function Resume(){
-return (
-<div>
+const proficiencies = [
+	{
+		name: "Client-Side",
+		skills: [
+			"HTML5",
+            "React",
+			"Bootstrap",
+			"CSS",
+			"JavaScript",
+			"Responsive Design"
+		]
+	}, {
+		name: "Server-Side",
+		skills: [
+			"NodeJS",
+			"Express",
+			"Session Storage",
+			"npm"
+		]
+	}, {
+		name: "Database Skills",
+		skills: [
+			"MySQL",
+			"Sequelize",
+			"NoSQL",
+			"SQLite",
+			"GraphQL",
+			"MongoDB",
+			"Mongoose"
+		]
+	},
+];
+export default function Resume() {
+  return (
     <div>
-        <h1>My Resume</h1>
-        <p><a class="btn btn-danger" role="button" href={myResume}
-   download="Olubukola_Abiona_Resume">
-  Download
-</a></p>
+    
+      
+      <div>
+      <Container>
+				<Row>
+					<Col>
+					<div className="jumbotron jumbotron-fluid">
+						<Container>
+							<h1 className="display-4 text-center">Resume</h1>
+						</Container>
+					</div>
+					</Col>
+				</Row>
+				<Row>
+					<Col className="text-center mb-3">
+                    <a
+            className="btn btn-primary"
+            role="button"
+            href={myResume}
+            download="Olubukola_Abiona_Resume"
+          >
+            Download
+          </a>
+						{/* <Button href={myResume}  download="Olubukola_Abiona_Resume">Download My Resume</Button> */}
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Accordion>
+							{proficiencies.map((skillCategory, evtKey) => {
+								return(
+								<Accordion.Item eventKey={evtKey}>
+									<AccordionHeader>{skillCategory.name}</AccordionHeader>
+									<Accordion.Body>
+									<ListGroup>
+										{skillCategory.skills.map((skill) => {
+											return (
+											<ListGroup.Item>{skill}</ListGroup.Item>
+											);
+										})}
+									</ListGroup>
+									</Accordion.Body>
+								</Accordion.Item>)
+							})}
+						</Accordion>
+					</Col>
+				</Row>
+			</Container>
+      </div>
     </div>
-
-</div>
-)
-};
+  );
+}
