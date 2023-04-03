ff900f1007246b9782a0e696f91d253a16039cf4
Olubukola Abiona <bukyabiona@gmail.com>
2023-04-03 19:24:59 -0300

    Updates

---
+++ precache-manifest.459bfc8734c1e8e10d206b53b6f23d62.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a50cbd8475ea015d3c3",
+    "url": "/static/js/main.4a50cbd8.chunk.js"
+  },
+  {
+    "revision": "098092f780b795f17964",
+    "url": "/static/css/1.760a644a.chunk.css"
+  },
+  {
+    "revision": "098092f780b795f17964",
+    "url": "/static/js/1.098092f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f8a63ea584d67352e70060be0ca3b71",
+    "url": "/static/media/mypix2.6f8a63ea.png"
+  },
+  {
+    "revision": "2abcc192c6fdee293c406b65cfffe866",
+    "url": "/static/media/events.2abcc192.jpg"
+  },
+  {
+    "revision": "08c2454d393b9ea982e4381dc577a6e4",
+    "url": "/static/media/fifa-world-cup.08c2454d.jpg"
+  },
+  {
+    "revision": "3fc5e33ffefb5e7721fcd12ef1a5e2e1",
+    "url": "/static/media/weather-dashboard.3fc5e33f.png"
+  },
+  {
+    "revision": "cb27d5f30e1cf1f14de1de46c5acfc2d",
+    "url": "/static/media/timed-quiz.cb27d5f3.png"
+  },
+  {
+    "revision": "edec7f848ebc8847a2bc27e8e71c2a94",
+    "url": "/static/media/pwd-gen.edec7f84.png"
+  },
+  {
+    "revision": "9af169499a9d55e28d0400be27b38095",
+    "url": "/static/media/code-editor.9af16949.jpg"
+  },
+  {
+    "revision": "fc04ed287507d7c9ffbf4d9ab5285e47",
+    "url": "/static/media/MyResume.fc04ed28.pdf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "e167c96ff9e0cb5a158230e7799597f0",
+    "url": "/index.html"
+  }
+];
